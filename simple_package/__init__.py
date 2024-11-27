276ca16b5332150f71cd77f9bd691709263f6000
Adam Young <adam.young.com@gmail.com>
2024-11-27 12:50:15 +0000

    Adding user interface for calculator

--- simple_package/__init__.py
+++ simple_package/__init__.py
@@ -1,5 +1,5 @@
 # Init file for the package
-from .statistics import stats
+# from .statistics import stats
 from .operations import add
 from .operations import subtract
 from .operations import divide
