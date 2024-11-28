59eee906dd7d32c6e9f0b19a12597478f1fd8278
Pelle Strandberg <pelle.strandberg1@gmail.com>
2024-11-28 16:39:07 +0000

    Few more changes

--- simple_package/operations.py
+++ simple_package/operations.py
@@ -2,25 +2,6 @@
 ## Basic online calculator
 ###
 
-## Here I have defined four functions for the four
-## basic operations. 
-##
-## 1) You should provide an interface
-## function that will prompt the user for input and
-## call the appropriate function based on the user's
-## input. The interface function should continue to
-## prompt the user for input until the user enters
-## 'exit'. 
-##
-## 2) The interface function should also handle
-## any exceptions that might be thrown by the basic
-## operations functions. If an exception is thrown,
-## the interface function should print an error message
-## and continue to prompt the user for input.
-##
-## 3) Add other "operations" to the calculator, that
-## involve complicated operations (e.g., trigonometric
-## functions, logarithms, etc.).
 
 import numpy as np
 
